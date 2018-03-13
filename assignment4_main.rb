8bbd3d1de54cfc2b32d717c593e7dc8945f54f3b
Arjun Kalburgi <askalburgi@gmail.com>
2018-03-13 12:58:45 -0600

    project start

---
+++ assignment4_main.rb
@@ -0,0 +1,11 @@
+# Project 4
+
+# See test files for execution instructions 
+
+# Group: Winter2018_Group1
+#     Bianca Angotti 
+#     Andrew McKernan
+#     Arjun Kalburgi
+
+# ConnectFour Application
+
