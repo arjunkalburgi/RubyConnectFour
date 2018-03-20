660b545a815fdd021fa548a334e3ab5b170c8598
Bianca Angotti <bangotti@ualberta.ca>
2018-03-19 23:38:27 -0600

    completed all board contracts

--- app/board/board.rb
+++ app/board/board.rb
@@ -2,48 +2,129 @@ require_relative './board_contracts'
 require_relative './board_components'
 
 class Board 
-    extend BoardContracts
-    attr_reader :dimensions, :add_piece
+    include BoardContracts
+    attr_reader :game_board, :rows, :columns
 
 
-    def initialize(dim) 
-        @dimensions = dim
-        @board = Array.new(@dimensions.rows){Array.new(@dimensions.columns, 0)}
+    def initialize(rows, columns) 
+        pre_init(rows, columns)
+
+        @rows = rows
+        @columns = columns
+        @game_board = Array.new(@rows){Array.new(@columns, 0)}
+
+        post_init
+        invariant
     end
-
-    def add_piece(column_number)
-        if can_add_to_column? column_number
-            # add piece 
-        end 
-    end 
-
-
-
-    private 
-
-    def check_win 
-        # return user that won 
-    end 
-
-    def clear_board 
-        # return the board to empty state
-    end
-
-
-
-    # trivial public functions 
-    public 
-
-    def print_board
-        # print board 
-    end 
 
     def can_add_to_column?(column_number)
-        # check column full
-    end 
+        invariant
+        pre_can_add_to_column(column_number)
+
+        # result = true
+
+        post_can_add_to_column
+        invariant
+        return result
+    end
 
     def available_columns
         # list of columns that can have a piece added to it 
+        invariant
+        pre_available_columns
+
+        # result = 
+
+        post_available_columns
+        invariant
+        return result
     end 
+
+    def add_piece(token, column_number)
+        invariant
+        pre_add_piece(column_number)
+
+        # result = return true or false
+
+        post_add_piece(column_number, token)
+        invariant
+        return result
+    end
+
+    def is_full?
+        invariant
+        pre_is_full
+
+        # result = true
+
+        post_is_full
+        invariant
+        return result
+    end
+
+    def row(row_number)
+        invariant
+        pre_row(row_number)
+
+        result = @game_board[row_number]
+
+        post_row
+        invariant
+        return result
+    end
+
+    def col(column_number)
+        invariant
+        pre_col(column_number)
+
+        # result = column_number
+
+        post_col
+        invariant
+        return result
+    end
+
+    def [](row, column)
+        invariant
+        pre_brackets(row, column)
+
+        result = @game_board[row][column]
+
+        post_brackets
+        invariant
+        return result
+    end
+
+    def to_s
+        invariant
+
+        @game_board.to_s
+        
+        invariant
+    end
+
+    def print_board
+        invariant
+        pre_print_board
+
+        line = ""
+        @game_board.each { |e|
+            line += "|" + e.join(" ") + "|\n"
+        }
+
+        post_print_board(line)
+        invariant
+        return line
+    end 
+
+    def clear_board 
+        invariant
+        pre_clear_board
+        
+        @game_board = Array.new(rows){Array.new(columns, 0)}
+
+        post_clear_board
+        invariant
+    end
 
 end 
