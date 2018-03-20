355d15bad74b3e9cd524ff33b4fae3c240e175ed
Arjun Kalburgi <askalburgi@gmail.com>
2018-03-19 22:34:54 -0600

    Player contracts and classes

--- app/player/player.rb
+++ app/player/player.rb
@@ -1,11 +1,18 @@
 
 
 class Player
+    attr_reader :player_name, :player_win_string, :player_piece_character
 
-    def initialize(colour)
-        @player_name 
-        @player_win_string
-        @player_piece_character
+    def initialize(name, win_string, piece_char)
+        invariant 
+        pre_init(win_string, piece_char)
+
+        @player_name = name
+        @player_win_string = win_string
+        @player_piece_character = piece_char
+
+        post_init
+        invariant         
     end
 
 end
